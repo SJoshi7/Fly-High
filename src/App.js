d74ae2e28475af737730b592dc04afd1128f6762
Sjoshi7 <sjoshise7@gmail.com>
2019-11-22 15:22:26 +0530

    router setup

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import Home from './pages/Home';
 import FlightDetails from './pages/FlightDetails';
 import Error from './pages/Error';
+import {Route,Switch} from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Home/>
-      <FlightDetails/>
-      <Error/>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route exact path="/flight-details/" component={FlightDetails}/>
+        <Route component={Error}/>
+      </Switch>  
     </>
   );
 }
