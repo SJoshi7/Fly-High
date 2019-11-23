6c371e2ad434d694b14d73c2ebe9ca659679f16e
Sjoshi7 <sjoshise7@gmail.com>
2019-11-23 12:06:45 +0530

    complete form

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -4,23 +4,78 @@ import Grid from '@material-ui/core/Grid';
 import Paper from '@material-ui/core/Paper';
 import { FaExchangeAlt } from 'react-icons/fa';
 import DateFnsUtils from '@date-io/date-fns';
+import jss from 'jss';
+import preset from 'jss-preset-default';
+import Button from '@material-ui/core/Button';
+import {typewriter} from '../helpers/typewriter';
 import {
   MuiPickersUtilsProvider,
   KeyboardTimePicker,
   KeyboardDatePicker,
 } from '@material-ui/pickers';
 
+jss.setup(preset());
 
-const form_box_style = {
-  padding:'5%',
-  marginTop:'10%',
-  marginRight:'10%',
-  marginLeft:'10%'
+
+const styles = {
+  boxFormStyle: {
+    margin:'10%',
+    padding:'5%',
+    border: '2px black solid',
+    boxShadow:'5px 5px white'
+  },
+  inputFormStyle:{
+    textAlign:'center'
+  },
+  exchangeIcon:{
+    fontSize: '1.5rem',
+    margin: '3%'
+  },
+  inputTextStyle:{
+    width:'40%'
+  },
+  rightInputTextStyle:{
+    marginLeft:'11%',
+    width:'40%'
+  },
+  datePickerStyle:{
+    width:'92%'
+  },
+  buttonStyle:{
+    backgroundColor:'#00F2A9',
+    marginTop:'10%'
+  },
+  '@media screen and (max-width: 768px)': {
+    boxFormStyle: {
+      margin:'5%',
+      border: '2px black solid',
+      boxShadow:'5px 5px white'
+    },
+    exchangeIcon:{
+      float:'right',
+      margin:'3%',
+      transform:'rotate(90deg)',
+      marginTop:'15%',
+      fontSize:'1.5rem'
+    },
+    inputTextStyle:{
+      width:'85%'
+    },
+    rightInputTextStyle:{
+      float:'none',
+      width:'85%',
+      marginLeft:'unset'
+    },
+    datePickerStyle:{
+      width:'85%'
+    },
+    inputFormStyle:{
+      textAlign:'unset'
+    }
+  }
 }
 
-const text_field_style = {
-  width:'43%'
-}
+const { classes } = jss.createStyleSheet(styles).attach();
 
 class InputForm extends Component{
   constructor(props){
@@ -35,6 +90,10 @@ class InputForm extends Component{
     }
   }
 
+  componentDidMount(){
+      typewriter();
+  }
+
   handleDateChange = value => {
     this.setState({
       selectedDate:value
@@ -45,43 +104,74 @@ class InputForm extends Component{
     return(
       <Grid container>
         <Grid item xs={12} sm>
-          left pane
+          <div id="typedtext">
+          </div>
         </Grid>
         <Grid item xs={12} sm>
-          <Paper style={form_box_style}>
-            <TextField
-              id="outlined-basic"
-              label="Source"
-              variant="outlined"
-              style={text_field_style}
-              margin="dense"
-            />
+          <Paper className={classes.boxFormStyle}>
+            <div className={classes.inputFormStyle}>
+              <TextField
+                className={classes.inputTextStyle}
+                required
+                id="standard-required"
+                label="Source"
+                margin="normal"
+              />
 
-            <FaExchangeAlt className="exchange-icon"/>
+              <FaExchangeAlt className={classes.exchangeIcon}/>
 
-            <TextField
-              id="outlined-basic"
-              label="Destination"
-              variant="outlined"
-              style={text_field_style}
-              margin="dense"
-            />
+              <TextField
+                className={classes.inputTextStyle}
+                required
+                id="standard-required"
+                label="Destination"
+                margin="normal"
+              />
 
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <KeyboardDatePicker
-                  disableToolbar
-                  variant="inline"
-                  format="MM/dd/yyyy"
-                  margin="normal"
-                  id="date-picker-inline"
-                  label="Date picker inline"
-                  value={this.state.selectedDate}
-                  onChange={this.handleDateChange}
-                  KeyboardButtonProps={{
-                    'aria-label': 'change date',
+              <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                  <KeyboardDatePicker
+                    className={classes.datePickerStyle}
+                    disableToolbar
+                    variant="inline"
+                    format="MM/dd/yyyy"
+                    margin="normal"
+                    id="date-picker-inline"
+                    label="Travel Date"
+                    fullWidth
+                    value={this.state.selectedDate}
+                    onChange={this.handleDateChange}
+                    KeyboardButtonProps={{
+                      'aria-label': 'change date',
+                    }}
+                  />
+              </MuiPickersUtilsProvider>
+              <div>
+                <TextField
+                  className={classes.inputTextStyle}
+                  id="standard-number"
+                  label="Passenger(s)"
+                  type="number"
+                  InputLabelProps={{
+                    shrink: true,
                   }}
+                  margin="normal"
+                  defaultValue="1"
                 />
-            </MuiPickersUtilsProvider>
+
+                <TextField
+                  className={classes.rightInputTextStyle}
+                  id="standard-required"
+                  label="Pay in (currency)"
+                  defaultValue="INR"
+                  margin="normal"
+                />
+              </div>
+            </div>
+            <center>
+              <Button variant="contained" className={classes.buttonStyle}>
+                Search Flights
+              </Button>
+            </center>
 
           </Paper>
         </Grid>
