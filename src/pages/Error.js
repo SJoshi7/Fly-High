b38ef808a28eaaa21e4990b5a1180af50fe8a0f2
Sjoshi7 <sjoshise7@gmail.com>
2019-11-25 11:20:07 +0530

    handeled bad urls

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,8 +1,30 @@
 import React from 'react';
+import jss from 'jss';
+import preset from 'jss-preset-default';
+import paragliding from '../images/paragliding.svg';
+jss.setup(preset());
+
+const styles={
+  imageStyle:{
+    width:'80px',
+    height:'80px'
+  },
+  errorMessage:{
+    fontSize:'2rem',
+    position:'absolute',
+    left:'25%',
+    top:'55%'
+  }
+}
+
+const { classes } = jss.createStyleSheet(styles).attach();
 
 const Error = () => {
   return(
-    <div>Error Encountered</div>
+    <div className="home">
+      <img src={paragliding} className={classes.imageStyle} id="paraglidingImage"/>
+      <h3 className={classes.errorMessage}>You Have Landed on a Wrong Page!</h3>
+    </div>
   );
 }
 
